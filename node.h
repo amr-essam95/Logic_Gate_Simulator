88923f4de4f83a27e2f88fdd7a6b5e53d167f224
Hatem <Mohamed.hatem1911@gmail.com>
2018-01-25 18:36:20 +0200

    Developing Node class

--- node.h
+++ node.h
@@ -1,6 +1,18 @@
 #ifndef NODE_H
 #define NODE_H
 
+class Node
+{
+	private:
+		int nodeValue;
+		int nodeId;
+	public:
+		int getNodeValue();
+		void setNodeValue(int val);
+		int getNodeId();
+		void setNodeId(int val);
+		void Node();
 
+};
 
 #endif
