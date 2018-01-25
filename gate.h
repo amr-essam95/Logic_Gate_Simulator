696531aa8e9c44319498b46d9cb31aed11735fbc
amr-essam95 <amr.essam95@hotmail.com>
2018-01-25 18:33:51 +0200

    Developing Gate Class

---
+++ gate.h
@@ -0,0 +1,20 @@
+#ifndef GATE_H
+#define GATE_H
+#include "node.h"
+
+class Gate
+{
+	Node *out;
+	Node *in1;
+	Node *in2;
+public:
+	Gate(Node *i1,Node *i2);
+	Node* getFirstInput();
+	Node* getSecondInput();
+	Node* getOutput();
+	void setFirstInput(Node* i);
+	void setSecondInput(Node* i);
+	void setOutput(Node* o);
+};
+
+#endif
