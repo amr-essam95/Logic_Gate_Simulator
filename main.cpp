0a333854d99ec362e82b3fa764190c5a1f4922b8
amr-essam95 <amr.essam95@hotmail.com>
2018-01-25 20:11:34 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -12,8 +12,16 @@ using namespace std;
 
 int main()
 {
-	Node x;
+	Node in1;
+	Node in2;
+	Node out;
 	Xor y;
+	y.setFirstInput(&in1);
+	y.setSecondInput(&in2);
+	y.setOutput(&out);
+	y.setFirstInput(1);
+	y.setSecondInput(0);
+	y.computeOutput();
 
 
 	return 0;
