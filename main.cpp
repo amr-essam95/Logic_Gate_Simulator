d912fe316b010a1a9311e32d99d09ffe33a43d3b
amr-essam95 <amr.essam95@hotmail.com>
2018-01-25 19:54:16 +0200

    testing in main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
-#include <iostream>
+#include "or.h"
+#include "not.h"
+#include "xor.h"
+#include "and.h"
+#include "xnor.h"
+#include "nand.h"
 #include "node.h"
 #include "gate.h"
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
 	Node x;
+	Xor y;
+
 
 	return 0;
 }
